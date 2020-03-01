76ca3414acff998e9a8dd0550cf65e558331c3fb
dylanindelab <dylan@indelab.fr>
2020-03-01 19:51:29 +0100

    ajout de toutes les pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,23 +7,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class HomeController extends AbstractController {
+
     /**
-     * @Route("/" ,name="home") 
+     * Montre la page qui m'intéressent
+     * 
+     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @Route("/bonjour", name="hello_base")
+     * @Route("bonjour/{prenom}", name="hello_prenom")
+     */
+    public function hello($prenom = "anonyme", $age = 0){
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+        );
+    }
+    /**
+     * @Route("/" ,name="homepage") 
      */
 
-     public function home() {
+    public function home() {
 
         $prenoms = ['Lior' => 31, 'Joseph' => 25, 'Anne' => 17];
          return $this->render( 
             'home.html.twig',
             [
-                'title' => "Bonjour à tous",
+                'title' => "Bonjour à tous !",
                 'age' => 18,
                 'tableau' => $prenoms
 
             ]
-         );
-     }
+        );
+    }
 }
 
 ?>
